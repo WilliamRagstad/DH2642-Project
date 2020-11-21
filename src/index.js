7f0731210ef2e6bb470d890cf29e835e113e7098
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2020-11-21 15:36:44 +0100

    Connected Redux DevTools

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import {createStore} from 'redux';
 import allReducers from "./reducers";
 
-const store = createStore(allReducers)
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
