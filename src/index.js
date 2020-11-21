99e29f563cd8432859d67abda4509afaf361e740
villed3 <villed3@outlook.com>
2020-11-21 15:48:24 +0100

    Added provider

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import './index.css';
 import App from './App';
 import {createStore} from 'redux';
 import allReducers from "./reducers";
+import { Provider } from 'react-redux';
 
 const store = createStore(
-  allReducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	allReducers,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
