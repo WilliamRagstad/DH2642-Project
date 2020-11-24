edc0f5fe60ea70e8c6b61922c28feae760aed7e5
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2020-11-24 18:29:16 +0100

    Firebase files to TypeScript

---
+++ src/firebase-config.ts
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyCurCsMsxjroxfjgt6LpCLhH2pfbgasIvI",
+    authDomain: "fir-project-e91a1.firebaseapp.com",
+    databaseURL: "https://fir-project-e91a1.firebaseio.com",
+    projectId: "fir-project-e91a1",
+    storageBucket: "fir-project-e91a1.appspot.com",
+    messagingSenderId: "276910270015",
+    appId: "1:276910270015:web:6026bc8036de178db63a8a"
+};
+
+export default config;
