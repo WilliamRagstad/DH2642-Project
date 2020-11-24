22890873a0a93e10b6df71c65b81ea52e4512999
villed3 <villed3@outlook.com>
2020-11-24 13:05:03 +0100

    Update counter-view.js

--- src/components/counter/counter-view.js
+++ src/components/counter/counter-view.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const LoginView = ({count, setCount, buttonText}) => 
+const CounterView = ({count, setCount, buttonText}) => 
 <React.Fragment>
     <p>You clicked {count} times.</p>
     <button onClick={() => setCount(count + 1)}>{buttonText}</button>
 </React.Fragment>;
 
-export default LoginView;
+export default CounterView;
