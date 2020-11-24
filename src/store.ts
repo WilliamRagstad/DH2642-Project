71ec0050b91567e3a307fc36033218799820a8cc
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2020-11-24 14:36:57 +0100

    Refactored all files to be TypeScript

---
+++ src/store.ts
@@ -0,0 +1,9 @@
+import { createStore } from 'redux';
+import allReducers from "./reducers";
+
+const store = createStore(
+	allReducers,
+	(window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
