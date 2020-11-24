5c55207f5e19df009c0bb5f67184be0630b5c1e6
villed3 <villed3@outlook.com>
2020-11-24 22:26:41 +0100

    Thunk with firebase fix

--- src/store.ts
+++ src/store.ts
@@ -3,15 +3,6 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import allReducers from "./reducers";
 
-// const enhancers = [
-// 	applyMiddleware(thunk),
-// 	reactReduxFirebase(firebaseConfig, {
-// 		userProfile: 'users',
-// 		useFirestoreForProfile: true,
-// 		attachAuthIsReady: true
-// 	})
-// ]
-
 const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
 	allReducers, composeEnhancers(applyMiddleware(thunk.withExtraArgument({getFirebase})))
