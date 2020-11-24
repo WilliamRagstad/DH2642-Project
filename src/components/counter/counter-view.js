c9aceeebb92a1eca5d456c1210d839f2ab96a664
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2020-11-24 13:51:24 +0100

    Refactor counter-view.js

--- src/components/counter/counter-view.js
+++ src/components/counter/counter-view.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const CounterView = ({count, setCount, buttonText}) => 
-<React.Fragment>
-    <p>You clicked {count} times.</p>
-    <button onClick={() => setCount(count + 1)}>{buttonText}</button>
-</React.Fragment>;
+const CounterView = ({ count, setCount, buttonText }) =>
+    <React.Fragment>
+        <p>You clicked {count} times.</p>
+        <button onClick={() => setCount(count + 1)}>{buttonText}</button>
+    </React.Fragment>;
 
 export default CounterView;
