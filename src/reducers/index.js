cb7d2603e6fa548e3331e7b00420323c392098a9
villed3 <villed3@outlook.com>
2020-11-24 14:03:11 +0100

    Added firebasereducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,13 @@
 import { combineReducers } from 'redux';
+import { firebaseReducer } from 'react-redux-firebase';
+
 import counterReducer from './counter/counter-reducer';
 import loggedReducer from './logged/isLogged-reducer';
 
 const allReducers = combineReducers({
-    counterReducer, loggedReducer
+    counterReducer,
+    loggedReducer,
+    firebase: firebaseReducer
 })
 
 export default allReducers;
