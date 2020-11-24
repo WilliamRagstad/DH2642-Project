2f029ebf946d1cced4ea1161dd2a655fa82999e4
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2020-11-24 13:51:52 +0100

    Implemented React-Redux counter from tutorial

--- src/App.js
+++ src/App.js
@@ -1,31 +1,41 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { Counter, LoadingIndicator } from './components';
+
+import { increment, decrement } from "./actions";
+
 import logo from './logo.svg';
 import './App.css';
 import './styles/sass/style.scss'
 
-import { Counter, LoadingIndicator } from './components';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      
-      <LoadingIndicator />
-      <Counter />
-    </div>
-  );
+
+	const counter = useSelector(state => state.counterReducer);
+	const dispatch = useDispatch();
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+        		</p>
+				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+					Learn React
+        		</a>
+			</header>
+
+			<LoadingIndicator />
+			<Counter />
+
+			<div>
+				<h1>Counter {counter}</h1>
+				<button onClick={() => dispatch(increment())}>+</button>
+				<button onClick={() => dispatch(decrement())}>-</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
