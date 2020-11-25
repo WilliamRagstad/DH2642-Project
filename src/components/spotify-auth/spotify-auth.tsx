664af1933cb37efe851073f149f9b9637aef66dd
Ludvig Lindén <ludvig978@gmail.com>
2020-11-25 15:22:08 +0100

    Open link in new tab

--- src/components/spotify-auth/spotify-auth.tsx
+++ src/components/spotify-auth/spotify-auth.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React from 'react';
 import config from '../../spotify-config';
 import { Card, CardHeader } from 'ui-neumorphism'
@@ -15,7 +16,7 @@ const SpotifyAuth = () => {
         <React.Fragment>
             <Card style={{margin: "5px", padding: "5px"}}>
                 <CardHeader>Login to Spotify</CardHeader>
-                <a href={`${authEndpoint}?client_id=${config.client_id}&redirect_uri=${config.redirect_uri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}>Login to Spotify</a>
+                <a href={`${authEndpoint}?client_id=${config.client_id}&redirect_uri=${config.redirect_uri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`} target="_blank">Login to Spotify</a>
             </Card>
         </React.Fragment>
     )
