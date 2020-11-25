401fba24ba13944329abed14503ee33ee558e0c8
villed3 <villed3@outlook.com>
2020-11-25 21:48:40 +0100

    Removed logged reducer

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -2,13 +2,11 @@ import { combineReducers } from 'redux';
 import { firebaseReducer } from 'react-redux-firebase';
 
 import counterReducer from './counter/counter-reducer';
-import loggedReducer from './logged/isLogged-reducer';
 import loginReducer from './auth/login-reducer';
 import signupReducer from './auth/signup-reducer';
 
 const allReducers = combineReducers({
     counterReducer,
-    loggedReducer,
     login: loginReducer,
     signup: signupReducer,
     firebase: firebaseReducer
