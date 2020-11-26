74b0e791351205a892595e700c8429c48d58209c
Ludvig Lindén <ludvig978@gmail.com>
2020-11-26 15:47:13 +0100

    Readded signout and moved more styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import { Button, Card, Tooltip, ProgressCircular } from 'ui-neumorphism'
+import { Button, Card, Tooltip } from 'ui-neumorphism'
 import 'ui-neumorphism/dist/index.css';
 
 import {
@@ -52,7 +52,11 @@ function App() {
 					<SpotifyAuth />
 				</Route>
 				<Route path="/">
-					root
+					<div className="flexparent">
+						{(isLoggedIn) ? <Button dark onClick={() => {
+								dispatch(signOut());
+							}}>Sign out</Button> : <Redirect to="/login" />}
+					</div>
 				</Route>
 			</Switch>
 		</Router>
