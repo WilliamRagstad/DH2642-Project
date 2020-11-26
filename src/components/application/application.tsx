354c391781702b54cb0849d140a78d4f1d8231c0
villed3 <villed3@outlook.com>
2020-11-26 18:41:55 +0100

    Update application.tsx

--- src/components/application/application.tsx
+++ src/components/application/application.tsx
@@ -7,18 +7,18 @@ const Application = () => {
             <div className="flex-child flex-parent">
                 <div className="application-sidebar flex-parent flex-column">
                     <div className="application-navigation flex-child">
-                        <Card inset className="fill-element" dark></Card>
+                        <Card inset bordered className="fill-element" dark></Card>
                     </div>
                     <div className="application-media-frame">
                         <img className="fill-element" alt="" src="https://i.ytimg.com/vi/vhl9wWLv2Yo/maxresdefault.jpg"/>
                     </div>
                 </div>
                 <div className="flex-child application-main">
-                    <Card className="fill-element" dark></Card>
+                    <Card bordered className="fill-element" dark></Card>
                 </div>
             </div>
             <div className="application-controls">
-                <Card elevation="2" className="fill-element" dark></Card>
+                <Card bordered className="fill-element" dark></Card>
             </div>
         </div>
     );
