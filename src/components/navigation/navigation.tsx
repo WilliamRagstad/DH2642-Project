254b634e6f138c4e32131d1b964dda724869dc85
villed3 <villed3@outlook.com>
2020-11-30 21:05:29 +0100

    Added some icons

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom';
 import { Card, Button, Divider } from 'ui-neumorphism';
 import { ReactComponent as SearchIcon } from '../../images/search-24px.svg';
 import { ReactComponent as HomeIcon } from '../../images/home-24px.svg';
+import { ReactComponent as SettingsIcon } from '../../images/settings-24px.svg';
+import { ReactComponent as PlaylistIcon } from '../../images/playlist-24px.svg';
+import { ReactComponent as LyricsIcon } from '../../images/lyrics-24px.svg';
 
 const Navigation = () => {
     const [active, setActive] = useState(0);
@@ -19,15 +22,15 @@ const Navigation = () => {
                 </Link>
                 <Divider dense/>
                 <Link to="/app/playlist">
-                    <Button dark rounded onClick={() => setActive(2)} active={active === 2}>Playlists</Button>
+                    <Button dark rounded onClick={() => setActive(2)} active={active === 2}><span className="button-icon-container"><PlaylistIcon fill="var(--light-bg)"/>&nbsp;</span>Playlists</Button>
                 </Link>
                 <Divider dense/>
                 <Link to="/app/lyrics">
-                    <Button dark rounded onClick={() => setActive(3)} active={active === 3}>Lyrics</Button>
+                    <Button dark rounded onClick={() => setActive(3)} active={active === 3}><span className="button-icon-container"><LyricsIcon fill="var(--light-bg)"/>&nbsp;</span>Lyrics</Button>
                 </Link>
                 <Divider dense/>
                 <Link to="/app/settings">
-                    <Button dark rounded onClick={() => setActive(4)} active={active === 4}>Settings</Button>
+                    <Button dark rounded onClick={() => setActive(4)} active={active === 4}><span className="button-icon-container"><SettingsIcon fill="var(--light-bg)"/>&nbsp;</span>Settings</Button>
                 </Link>
             </Card>
         </React.Fragment>
