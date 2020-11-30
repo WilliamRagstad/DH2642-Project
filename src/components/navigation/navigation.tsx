e323e3158948cdc6a7cc5e7ed2d9b65625919ab1
Ludvig Lindén <ludvig978@gmail.com>
2020-11-30 15:44:59 +0100

    Added routes in maincontent component

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Card, Button, Divider } from 'ui-neumorphism';
 import { ReactComponent as SearchIcon } from '../../images/search-24px.svg';
 import { ReactComponent as HomeIcon } from '../../images/home-24px.svg';
@@ -12,6 +13,10 @@ const Navigation = () => {
                 <Button dark rounded><span className="button-icon-container"><SearchIcon fill="var(--light-bg)"/>&nbsp;</span>Search</Button>
                 <Divider dense/>
                 <Button dark rounded>Playlists</Button>
+                <Divider dense/>
+                <Link to="/app/lyrics">
+                    <Button dark rounded>Lyrics</Button>
+                </Link>
                 <Divider dense/>
                 <Button dark rounded>Settings</Button>
             </Card>
