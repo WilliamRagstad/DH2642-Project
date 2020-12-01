171803c79502eea1c08ef27bfa424f44fa183f80
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2020-12-01 13:37:59 +0100

    Refactor settings

--- src/components/main-content/main-content.tsx
+++ src/components/main-content/main-content.tsx
@@ -6,35 +6,35 @@ import LyricsView from './lyrics/lyrics-view';
 import SettingsView from "./settings/settings-view";
 
 const MainContent = () => {
-    return (
-        <React.Fragment>
-          <Switch>
-            <Route path="/app/home">
-              <HomeView/>
-            </Route>
-              
-            <Route path="/app/playlist">
-              <PlaylistView/>
-            </Route>
+	return (
+		<React.Fragment>
+			<Switch>
+				<Route path="/app/home">
+					<HomeView />
+				</Route>
 
-            <Route path="/app/lyrics">
-              <LyricsView/>
-            </Route>
+				<Route path="/app/playlist">
+					<PlaylistView />
+				</Route>
 
-            <Route path="/app/settings">
-				<SettingsView/>
-            </Route>
+				<Route path="/app/lyrics">
+					<LyricsView />
+				</Route>
 
-            <Route path="/app/search">
+				<Route path="/app/settings">
+					<SettingsView />
+				</Route>
 
-            </Route>
+				<Route path="/app/search">
 
-            <Route path="/app/" exact>
-              <Redirect to="/app/home"/>
-            </Route>
-          </Switch>
-        </React.Fragment>
-    );
+				</Route>
+
+				<Route path="/app/" exact>
+					<Redirect to="/app/home" />
+				</Route>
+			</Switch>
+		</React.Fragment>
+	);
 }
 
 export default MainContent;
