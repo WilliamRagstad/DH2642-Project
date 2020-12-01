1f327f23a8f3ab4321250d828d1848b6e8bdafde
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2020-12-01 21:50:39 +0100

    Added connect to save Spotify in Firestore

--- src/components/connect/connect.tsx
+++ src/components/connect/connect.tsx
@@ -1,34 +1,43 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 import { useDispatch } from "react-redux";
+import firebase from '../../firebase';
+import "firebase/firestore";
+
 import { connect } from '../../actions/connect-actions';
 import { ISpotify } from '../../interfaces';
+
+import { parseParams } from "../../helpers/parsing";
+import { getCurrentUser } from '../../helpers/firebase';
 
 const Connect = ({ match, props, location }) => {
     const service = match.params.service;
     const dispatch = useDispatch();
+    const db = firebase.firestore();
+
     // console.log(match)
     // console.log(location)
 
     switch (service) {
         case "spotify":
-            const data: ISpotify = location.hash.slice(1).split('&').reduce((obj, item) => {
-                const i = item.split('=');
-                return {
-                    ...obj,                                     // Concat with result object
-                    [i[0]]: !isNaN(i[1]) ? Number(i[1]) : i[1]  // Cast right hand side to a number if it is a raw number, else string
-                }
-            }, {});
-            if (!data.access_token || !data.token_type || !data.expires_in) return <h1>Invalid service parameters</h1>;
-            data.connected = true;
-            dispatch(connect("SPOTIFY", data));
+            const hashdata = parseParams(location.hash.slice(1));
+            if (hashdata.access_token && hashdata.token_type && hashdata.expires_in) {
+                // Save to Cloud Firestore
+                getCurrentUser(firebase.auth()).then((user: any) => {
+                    db.collection("services").doc("spotify").collection("users").doc(user.uid).set(hashdata).then(function () {
+                        hashdata.connected = true;
+                        dispatch(connect("SPOTIFY", hashdata as ISpotify));
+                    }).catch(console.error)
+                })
+
+            }
             break;
 
         default:
             return <h1>No such service found.</h1>;
     }
 
-    return <Redirect to="/app" />; // <h1>You are connecting to {service}...</h1>;
+    return <Redirect to="/app" />;
 }
 
 export default Connect;
