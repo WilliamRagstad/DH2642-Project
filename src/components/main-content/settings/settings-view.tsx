e598e2c5c238c53a3f46933a175797529cfb237d
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2020-12-02 15:31:36 +0100

    Added rounded to settings buttons

--- src/components/main-content/settings/settings-view.tsx
+++ src/components/main-content/settings/settings-view.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useSelector } from "react-redux";
-import { Card, CardContent, CardHeader, Divider, Tab, Tabs, TabItem, TabItems, Button, Switch, ToggleButtonGroup, ToggleButton, RadioGroup, Radio } from 'ui-neumorphism';
+import { Card, CardContent, CardHeader, Divider, Tab, Tabs, TabItem, TabItems, Button, Switch, ToggleButtonGroup, ToggleButton, RadioGroup, Radio, IconButton } from 'ui-neumorphism';
 import SpotifyAuth from "../../spotify-auth/spotify-auth";
 import { useDispatch } from 'react-redux';
 import { signOut } from "../../../actions/login-actions";
@@ -50,24 +50,24 @@ const SettingsView = () => {
                                 </div>
                                 <div className="setting-separated">
                                     Dark theme
-                                    <Switch dark color='var(--primary)' checked={current_ui.theme.dark} onClick={e => applyThemeDark(e.target.checked, current_ui)}/>
+                                    <Switch dark color='var(--primary)' checked={current_ui.theme.dark} onClick={e => applyThemeDark(e.target.checked, current_ui)} />
                                 </div>
                                 <div className="setting-separated">
                                     Primary color
                                     <ToggleButtonGroup mandatory dark value={current_ui.theme.primary}>
-                                        <ToggleButton value='blue' onClick={() => applyThemePrimary('blue', current_ui)}>
+                                        <ToggleButton rounded value='blue' onClick={() => applyThemePrimary('blue', current_ui)}>
                                             <LensIcon fill="var(--theme-blue)" className="label-icon" />
                                         </ToggleButton>
-                                        <ToggleButton value='orange' onClick={() => applyThemePrimary('orange', current_ui)}>
+                                        <ToggleButton rounded value='orange' onClick={() => applyThemePrimary('orange', current_ui)}>
                                             <LensIcon fill="var(--theme-orange)" className="label-icon" />
                                         </ToggleButton>
-                                        <ToggleButton value='green' onClick={() => applyThemePrimary('green', current_ui)}>
+                                        <ToggleButton rounded value='green' onClick={() => applyThemePrimary('green', current_ui)}>
                                             <LensIcon fill="var(--theme-green)" className="label-icon" />
                                         </ToggleButton>
-                                        <ToggleButton value='red' onClick={() => applyThemePrimary('red', current_ui)}>
+                                        <ToggleButton rounded value='red' onClick={() => applyThemePrimary('red', current_ui)}>
                                             <LensIcon fill="var(--theme-red)" className="label-icon" />
                                         </ToggleButton>
-                                        <ToggleButton value='pink' onClick={() => applyThemePrimary('pink', current_ui)}>
+                                        <ToggleButton rounded value='pink' onClick={() => applyThemePrimary('pink', current_ui)}>
                                             <LensIcon fill="var(--theme-pink)" className="label-icon" />
                                         </ToggleButton>
                                     </ToggleButtonGroup>
@@ -81,7 +81,10 @@ const SettingsView = () => {
                             <CardContent>
                                 <div className="setting-separated">
                                     {connectedText(spotifyConnected, "Spotify")}
-                                    <SpotifyAuth enabled={!spotifyConnected} />
+                                    <div>
+                                        <SpotifyAuth enabled={!spotifyConnected} />
+                                        <IconButton dark rounded><CrossIcon fill='var(--error)' /></IconButton>
+                                    </div>
                                 </div>
                             </CardContent>
                         </Card>
