352ee233ec384975493ea030de899029aa4d77f4
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2020-12-02 16:44:17 +0100

    Refactor Firestore wrapper

--- src/components/firestore-loader/service-loader.tsx
+++ src/components/firestore-loader/service-loader.tsx
@@ -4,20 +4,16 @@ import "firebase/firestore";
 
 import { ISpotify } from '../../interfaces';
 import { connect } from '../../actions/connect-actions';
-import { getCurrentUser } from '../../helpers/firebase';
+import { loadFirestoreUserData } from '../../helpers/firebase';
 
 export function LoadServices() {
     const dispatch = useDispatch();
-    const db = firebase.firestore();
 
-    getCurrentUser(firebase.auth()).then((user: any) => {
-        if (!user) return;
-        db.collection("services").doc("spotify").collection("users").doc(user.uid).get().then((snapshot) => {
-            if (snapshot.exists) {
-                const hashdata = snapshot.data();
-                hashdata.connected = true;
-                dispatch(connect("SPOTIFY", hashdata as ISpotify));
-            }
-        });
-    })
+    loadFirestoreUserData("services", "spotify").then((snapshot) => {
+        if (snapshot.exists) {
+            const hashdata = snapshot.data();
+            hashdata.connected = true;
+            dispatch(connect("SPOTIFY", hashdata as ISpotify));
+        }
+    });
 }
--- src/helpers/firebase.ts
+++ src/helpers/firebase.ts
@@ -3,7 +3,7 @@ import "firebase/firestore";
 
 const db = firebase.firestore();
 
-export function getCurrentUser(auth) {
+function getCurrentUser(auth) {
     return new Promise((resolve, reject) => {
         const unsubscribe = auth.onAuthStateChanged(user => {
             unsubscribe();
@@ -17,4 +17,7 @@ export function saveFirestoreUserData(collection: string, doc: string, data: any
 }
 export function loadFirestoreUserData(collection: string, doc: string) {
     return getCurrentUser(firebase.auth()).then((user: any) => db.collection(collection).doc(doc).collection("users").doc(user.uid).get())
+}
+export function deleteFirestoreUserData(collection: string, doc: string) {
+    return getCurrentUser(firebase.auth()).then((user: any) => db.collection(collection).doc(doc).collection("users").doc(user.uid).delete())
 }
