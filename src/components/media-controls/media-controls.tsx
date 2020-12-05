93f6198510dbb2dd35149d85a3d4bfb93ad331a5
villed3 <villed3@outlook.com>
2020-12-05 21:36:17 +0100

    Added Spotify connect player

--- src/components/media-controls/media-controls.tsx
+++ src/components/media-controls/media-controls.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import SpotifyPlayer from './spotify-player';
 import { Card } from 'ui-neumorphism';
+import { useSelector } from 'react-redux';
+import IState from '../../interfaces/redux/state';
 
 const MediaControls = () => {
+
+    const isLoggedIn = useSelector((state: IState) => state.firebase.auth.isLoaded && !state.firebase.auth.isEmpty);
+    const isConnectedToSpotify = useSelector((state: IState) => state.spotify.connected);
 
     return (
         <React.Fragment>
             <Card className="fill-element">
+                {isLoggedIn && isConnectedToSpotify && <SpotifyPlayer/>}
             </Card>
-        </React.Fragment>
-        
+        </React.Fragment>        
     )
 }
 export default MediaControls;
