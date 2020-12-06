f43acbc1ae0ab10114401421d2dd5f41a342be61
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2020-12-06 23:26:31 +0100

    Fixed lavastore integration

--- src/components/connect/connect.tsx
+++ src/components/connect/connect.tsx
@@ -9,7 +9,7 @@ import { parseParams } from "../../helpers/parsing";
 import { saveFirestoreUserData } from '../../helpers/firebase';
 
 import spotifyConfig from '../../spotify-config';
-import { StorageDocument } from '../../helpers/storage';
+import { LSDocument } from 'lavastore';
 
 import axios from 'axios';
 
@@ -28,10 +28,10 @@ const Connect = ({ match, props, location }) => {
                 const urlParams = new URLSearchParams(location.search);
                 if (urlParams.get('code')) {
                     const code = urlParams.get('code');
-                    const sd = new StorageDocument('spotify-verifier');
+                    const sd = new LSDocument('spotify-verifier');
                     sd.Load();
                     const v = (sd.Get() as any)?.verifier;
-                     
+
                     axios.post('https://accounts.spotify.com/api/token', null, {
                         params: {
                             grant_type: 'authorization_code',
@@ -44,34 +44,34 @@ const Connect = ({ match, props, location }) => {
                             'Content-Type': 'application/x-www-form-urlencoded'
                         }
                     })
-                    .then(response => {
-                        if (response.status !== 200) {
+                        .then(response => {
+                            if (response.status !== 200) {
+                                console.log(response);
+                                throw new Error("HTTP error: " + response.status);
+                            }
                             console.log(response);
-                            throw new Error("HTTP error: " + response.status);
-                        }
-                        console.log(response);
-                        return response.data;
-                    })
-                    .then(data => {
-                        console.log(data);
-                        // Save to Cloud Firestore
-                        saveFirestoreUserData("services", "spotify", data).then(function () {
-                            hashdata.connected = true;
-                            dispatch(connect("SPOTIFY", data as ISpotify));
-                        }).catch(console.error)
-                    })
-                    .catch(err => {
-                        console.log(err.response);
-                        console.log(err.request);
-                        console.log(err.message);
-                        console.log('Could not retrieve access key');
-                    });
-    
+                            return response.data;
+                        })
+                        .then(data => {
+                            console.log(data);
+                            // Save to Cloud Firestore
+                            saveFirestoreUserData("services", "spotify", data).then(function () {
+                                hashdata.connected = true;
+                                dispatch(connect("SPOTIFY", data as ISpotify));
+                            }).catch(console.error)
+                        })
+                        .catch(err => {
+                            console.log(err.response);
+                            console.log(err.request);
+                            console.log(err.message);
+                            console.log('Could not retrieve access key');
+                        });
+
                 } else if (urlParams.get('error')) {
                     console.log(urlParams.get('error'));
                 }
                 break;
-    
+
             default:
                 return <h1>No such service found.</h1>;
         }
