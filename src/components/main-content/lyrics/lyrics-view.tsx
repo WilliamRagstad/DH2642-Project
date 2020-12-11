295c08e4c3afb2fa2e6723e4b0069f8faff86b95
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2020-12-11 21:50:45 +0100

    Disable Currently playing button if no playing

--- src/components/main-content/lyrics/lyrics-view.tsx
+++ src/components/main-content/lyrics/lyrics-view.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { connect } from 'react-redux';
+import { connect, useSelector } from 'react-redux';
 import { Button, Card, CardContent, CardHeader, TextField, ProgressLinear, Subtitle1 } from 'ui-neumorphism';
 import actions from '../../../actions';
+import { IState } from '../../../interfaces';
 
 const LyricsView = ({
     sameAsPlaying,
@@ -13,9 +14,10 @@ const LyricsView = ({
     getLyricsFromId,
     searchLyrics,
     currentForeignId
-}) => {    
+}) => {
     const [query, setQuery] = useState("");
     const [cachedForeignId, setCachedForeignId] = useState("");
+    const isPlaying = useSelector((state: IState) => state.media.isPlaying);
 
     useEffect(() => {
         if (currentForeignId !== cachedForeignId && sameAsPlaying) {
@@ -37,51 +39,51 @@ const LyricsView = ({
         <React.Fragment>
             <Card className="view-card flex-parent flex-column float-container" inset rounded>
                 <CardHeader>Lyrics</CardHeader>
-                    <div className="flex-parent flex-align-center lyrics-search-form" spellCheck="false">
-                        <Button onClick={() => getCurrentLyrics()} depressed active={sameAsPlaying}>Currently playing</Button>
-                        <span>OR</span>
-                        <form autoComplete="off" onSubmit={e => {
-                            e.preventDefault();
-                            if (query) searchLyrics(query);
-                        }}>
-                            <TextField bordered placeholder="Search for song..." className="lyrics-search-field" hideExtra onInput={e => setQuery((e.target as HTMLInputElement).value.trim())} type="text"></TextField>
-                        </form>
-                        <Button onClick={() => {
-                            if (query) searchLyrics(query);
-                        }}>Search</Button>
-                    </div>
-                <ProgressLinear indeterminate={isLoading} color='var(--primary)' value={0} height={6}/>
+                <div className="flex-parent flex-align-center lyrics-search-form" spellCheck="false">
+                    <Button onClick={() => getCurrentLyrics()} depressed active={sameAsPlaying} disabled={!isPlaying}>Currently playing</Button>
+                    <span>OR</span>
+                    <form autoComplete="off" onSubmit={e => {
+                        e.preventDefault();
+                        if (query) searchLyrics(query);
+                    }}>
+                        <TextField bordered placeholder="Search for song..." className="lyrics-search-field" hideExtra onInput={e => setQuery((e.target as HTMLInputElement).value.trim())} type="text"></TextField>
+                    </form>
+                    <Button onClick={() => {
+                        if (query) searchLyrics(query);
+                    }}>Search</Button>
+                </div>
+                <ProgressLinear indeterminate={isLoading} color='var(--primary)' value={0} height={6} />
                 <CardContent className="lyrics-container">
-                    { id ? 
-                    <Card className="float-item lyrics-content">
-                        <CardHeader>
-                            {title} { url ? <Button text color='var(--primary)'>
-                                <a href={url} rel="noreferrer" target="_blank">Source</a>
-                            </Button> : ""}
-                            <Subtitle1 secondary>{artist}</Subtitle1>
-                        </CardHeader>
-                        <CardContent>
-                            {lyricsError && !isLoading ?
-                            <React.Fragment>
-                                {lyricsError} &nbsp;
+                    {id ?
+                        <Card className="float-item lyrics-content">
+                            <CardHeader>
+                                {title} {url ? <Button text color='var(--primary)'>
+                                    <a href={url} rel="noreferrer" target="_blank">Source</a>
+                                </Button> : ""}
+                                <Subtitle1 secondary>{artist}</Subtitle1>
+                            </CardHeader>
+                            <CardContent>
+                                {lyricsError && !isLoading ?
+                                    <React.Fragment>
+                                        {lyricsError} &nbsp;
                                 <Button depressed onClick={() => getLyricsFromId(id)}>Reload</Button>
-                            </React.Fragment>
-                            : lyrics}
-                        </CardContent>
-                    </Card>
-                    : searchResults.length >= 1 && 
-                    <div className="lyrics-result-container flex-parent">
-                        {searchResults.map(song => (
+                                    </React.Fragment>
+                                    : lyrics}
+                            </CardContent>
+                        </Card>
+                        : searchResults.length >= 1 &&
+                        <div className="lyrics-result-container flex-parent">
+                            {searchResults.map(song => (
                                 <div key={song.id} onClick={() => getLyricsFromId(song.id)} className="lyrics-result-item">
-                                    <img src={song.albumArtUrl} alt=""/>
+                                    <img src={song.albumArtUrl} alt="" />
                                     <div className="lyrics-result-text">
                                         <h4 className="title"><span>{song.title}</span></h4>
                                         <p><span>{song.artist}</span></p>
-                                    </div>                                    
+                                    </div>
                                 </div>
                             )
-                        )}
-                    </div>
+                            )}
+                        </div>
                     }
                 </CardContent>
             </Card>
