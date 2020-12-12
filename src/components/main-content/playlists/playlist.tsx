c052cd1bf6091572350febd68c55db0f894a4140
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2020-12-12 16:04:30 +0100

    Circle play icon in playlist

--- src/components/main-content/playlists/playlist.tsx
+++ src/components/main-content/playlists/playlist.tsx
@@ -19,7 +19,7 @@ const Playlists = ({
     activePlaylist,
     getSpotifyPlaylist
 }) => {
-    
+
     const playlistService = match.params.service;
     const playlistId = match.params.id;
     const tableHeaders = useRef(null);
@@ -29,10 +29,10 @@ const Playlists = ({
         switch (playlistService) {
             case 'spotify':
                 tableHeaders.current = [
-                    {text: 'Title', value: 'title'},
-                    {text: 'Artist', value: 'artist'},
-                    {text: 'Album', value: 'album'},
-                    {text: 'Length', value: 'duration'}
+                    { text: 'Title', value: 'title' },
+                    { text: 'Artist', value: 'artist' },
+                    { text: 'Album', value: 'album' },
+                    { text: 'Length', value: 'duration' }
                 ];
                 if (!activePlaylist || playlistId !== activePlaylist.id) getSpotifyPlaylist(playlistId);
                 break;
@@ -49,7 +49,7 @@ const Playlists = ({
         <React.Fragment>
             <Card className="view-card float-container flex-parent flex-column" inset>
                 <CardHeader className="playlist-header">
-                    {playlistService === 'spotify' ? <img src={spotifyIcon} alt="" height="24px"/> : ''}
+                    {playlistService === 'spotify' ? <img src={spotifyIcon} alt="" height="24px" /> : ''}
                     <span>{activePlaylist.name}</span>
                     <Button onClick={() => playPlaylist(playlistService, playlistId)} color='var(--primary)'>Play</Button>
                 </CardHeader>
@@ -58,21 +58,23 @@ const Playlists = ({
                 </CardContent>
                 <div><Divider className="playlists-divider" dense /></div>
                 <div className="playlist-list flex-child flex-parent flex-column">
-                    {activePlaylist.tracks ? (playlistService === 'spotify' ? activePlaylist.tracks.map(track => { return (
-                            <div className="playlist-track flex-child" key={ track.id }>
+                    {activePlaylist.tracks ? (playlistService === 'spotify' ? activePlaylist.tracks.map(track => {
+                        return (
+                            <div className="playlist-track flex-child" key={track.id}>
                                 <div className="playlist-track-play">
-                                    <IconButton size="small">
-                                        <PlayIcon fill='var(--g-text-color-light)'/>
+                                    <IconButton size="small" rounded>
+                                        <PlayIcon fill='var(--g-text-color-light)' />
                                     </IconButton>
                                 </div>
-                                <div className="playlist-track-title">{ track.title }</div>
-                                <div className="playlist-track-secondary">{ track.artists.map((artist, index) => {
+                                <div className="playlist-track-title">{track.title}</div>
+                                <div className="playlist-track-secondary">{track.artists.map((artist, index) => {
                                     return (index ? ', ' : '') + artist.name
                                 })}</div>
                                 <div className="playlist-track-tertiary">{track.album.name}</div>
                                 <div className="playlist-track-duration">{songLength(track.duration)}</div>
                             </div>
-                    )}) : 'Empty playlist.') : 'Invalid service.'}
+                        )
+                    }) : 'Empty playlist.') : 'Invalid service.'}
                 </div>
             </Card>
         </React.Fragment>
