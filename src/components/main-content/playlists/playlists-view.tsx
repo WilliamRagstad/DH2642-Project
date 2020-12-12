8d4131f4b1872da7fcfbcdd8bc0761ab05420fe2
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2020-12-13 00:01:17 +0100

    Added SoundBundle icon to playlists

--- src/components/main-content/playlists/playlists-view.tsx
+++ src/components/main-content/playlists/playlists-view.tsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { Button, Card, CardHeader, CardContent, Table, Divider, CardMedia, Subtitle1, Subtitle2 } from 'ui-neumorphism';
 import actions from '../../../actions';
 import IState from '../../../interfaces/redux/state';
+import soundBundleIcon from '../../../images/soundBundle.png';
 import spotifyIcon from '../../../images/Spotify_Icon_RGB_Green.png';
 import youtubeIcon from '../../../images/youtube_icon.png';
 import { Link } from 'react-router-dom';
@@ -37,17 +38,18 @@ const PlaylistsView = ({
                     <CardContent className="playlists-content flex-parent flex-column">
                         <div className="playlists-category">
                             <div className="playlists-category-header">
+                                <img src={soundBundleIcon} alt="" height="24px" />
                                 <span className="playlists-category-title">SoundBundle Mixed Playlists</span>
                                 <Link to="/app/playlists/mixed">
                                     <Button color="var(--primary)" text>Show All</Button>
                                 </Link>
                             </div>
-                            <Divider dense/>
+                            <Divider dense />
                             <div className="playlists-preview-container">
-                            </div>                      
-                            
+                            </div>
+
                         </div>
-                        { connectedToSpotify ? <div className="playlists-category">
+                        {connectedToSpotify ? <div className="playlists-category">
                             <div className="playlists-category-header">
                                 <img src={spotifyIcon} alt="" height="24px" />
                                 <span className="playlists-category-title">
@@ -57,12 +59,12 @@ const PlaylistsView = ({
                                     <Button color="var(--primary)" text>Show All</Button>
                                 </Link>
                             </div>
-                            <Divider dense/>
-                            { (spotifyPlaylists !== undefined && spotifyPlaylists.length > 0) ? (
-                                    <div className="playlists-preview-container playlists-items">
+                            <Divider dense />
+                            {(spotifyPlaylists !== undefined && spotifyPlaylists.length > 0) ? (
+                                <div className="playlists-preview-container playlists-items">
                                     {spotifyPlaylists.map(playlist => (
                                         <Link key={playlist.id} to={`/app/playlists/spotify/${playlist.id}`} className="playlists-item">
-                                            <img src={playlist.image} alt=""/>
+                                            <img src={playlist.image} alt="" />
                                             <div className="playlists-item-text">
                                                 <h4>
                                                     {playlist.name}
@@ -73,8 +75,8 @@ const PlaylistsView = ({
                                             </div>
                                         </Link>
                                     ))}
-                                    </div>
-                                ) : ''}
+                                </div>
+                            ) : ''}
                         </div> : ''}
                         {true ? <div className="playlists-category">
                             <div className="playlists-category-header">
