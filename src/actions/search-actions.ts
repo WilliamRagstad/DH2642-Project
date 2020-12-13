19d3624384a6e3f5957c7b72c5e0fb11eedd90c0
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2020-12-13 02:50:48 +0100

    Fixed searchTrack accept array and saved search services in localStorage

--- src/actions/search-actions.ts
+++ src/actions/search-actions.ts
@@ -1,27 +1,21 @@
 import spotify from '../spotify';
 import { validateSpotifyToken } from '../helpers/spotify';
 
-export const searchTrack = (service: string, query: string) => {
-    return (dispatch, getstate) => {
-        switch (service) {
-            case 'spotify':
-                spotifySearch(dispatch, query, 10);
-                break;
+export const searchTrack = (services: string[], query: string) => {
+    return (dispatch, getState) => {
+        if (services.length === 0) dispatch({ type: 'SEARCH_ERROR', payload: "Invalid service, try again." });
+        else services.forEach(service => {
+            switch (service) {
+                case 'spotify':
+                    spotifySearch(dispatch, query, 10);
+                    break;
 
-            case 'youtube':
-                dispatch({ type: 'SEARCH_SUCCESS' });
-                dispatch({ type: 'SET_SEARCH_RESULTS', payload: [] })
-                break;
-
-            case 'both':
-                spotifySearch(dispatch, query, 3);
-                break;
-
-            default:
-                dispatch({ type: 'SEARCH_ERROR', payload: "Invalid service, try again." })
-                break;
-        }
-
+                case 'youtube':
+                    dispatch({ type: 'SEARCH_SUCCESS' });
+                    dispatch({ type: 'SET_SEARCH_RESULTS', payload: [] })
+                    break;
+            }
+        });
     }
 }
 
@@ -29,11 +23,11 @@ function spotifySearch(dispatch: any, query: string, limit: number) {
     validateSpotifyToken().then(async () => {
         dispatch({ type: 'SET_SEARCH_LOADING' });
         const songs = await spotify.searchTracks(query, { limit: limit }).then(result => {
-            if(result.tracks.items.length >= 1){
+            if (result.tracks.items.length >= 1) {
                 dispatch({ type: 'SEARCH_SUCCESS' });
                 return result;
             }
-            else{
+            else {
                 dispatch({ type: 'SEARCH_ERROR', payload: "No results found. Check spelling and try again." })
                 return result;
             }
--- src/components/main-content/search/search-view.tsx
+++ src/components/main-content/search/search-view.tsx
@@ -6,6 +6,7 @@ import spotifyIcon from '../../../images/Spotify_Icon_RGB_Green.png';
 import youtubeIcon from '../../../images/youtube_icon.png';
 import { PlayIcon, PauseIcon, TimeIcon } from '../../icons/icons';
 import { IState } from '../../../interfaces';
+import { AppDocument } from '../../../lavastore';
 
 const SearchView = ({
     isLoading,
@@ -18,7 +19,6 @@ const SearchView = ({
     pausePlay
 }) => {
     const [query, setQuery] = useState("");
-    const [service, setService] = useState("");
 
     const headers = [
         { text: 'Title', align: 'left', value: 'title' },
@@ -29,6 +29,14 @@ const SearchView = ({
 
     const [spotifyConnected, youtubeConnected] = useSelector((state: IState) => [state.spotify.connected, true]);
 
+    let searchServices = (AppDocument.DocumentPath("cache/search")?.Get() as any)?.services as string[] ?? [];
+    function toggleService(service: string) {
+        if (searchServices.includes(service)) searchServices = searchServices.filter(s => s !== service);
+        else searchServices = [...searchServices, service];
+
+        AppDocument.SetPath("cache/search", { services: searchServices });
+    }
+
     return (
         <React.Fragment>
             <Card className="view-card float-container flex-parent flex-column" inset rounded>
@@ -36,38 +44,22 @@ const SearchView = ({
                 <div className="flex-parent flex-align-center search-form" spellCheck="false">
                     <form autoComplete="off" onClick={e => {
                         e.preventDefault();
-                        if (query) searchTrack(service, query);
+                        if (query) searchTrack(searchServices, query);
                     }}>
                         <TextField bordered placeholder="Search for song..." className="search-field" hideExtra onInput={e => setQuery((e.target as HTMLInputElement).value.trim())} type="text"></TextField>
 
                         <input type="submit" hidden />
                     </form>
-                    <ToggleButtonGroup multiple>
-                        <ToggleButton className="search-toggleButtons" value='spotify' disabled={!spotifyConnected} onClick={() => {
-                            if (service === "youtube")
-                                setService("both");
-                            else if (service === "both")
-                                setService("youtube");
-                            else if (service === "spotify")
-                                setService("");
-                            else setService("spotify");
-                        }}>
+                    <ToggleButtonGroup multiple value={searchServices}>
+                        <ToggleButton className="search-toggleButtons" value='spotify' disabled={!spotifyConnected} onClick={e => toggleService(e.value)}>
                             <img alt="" height="24px" src={spotifyIcon} />
                         </ToggleButton>
-                        <ToggleButton className="search-toggleButtons" value='youtube' disabled={!youtubeConnected} onClick={() => {
-                            if (service === "spotify")
-                                setService("both");
-                            else if (service === "both")
-                                setService("spotify");
-                            else if (service === "youtube")
-                                setService("");
-                            else setService("youtube");
-                        }}>
+                        <ToggleButton className="search-toggleButtons" value='youtube' disabled={!youtubeConnected} onClick={e => toggleService(e.value)}>
                             <img alt="" height="24px" src={youtubeIcon} />
                         </ToggleButton>
                     </ToggleButtonGroup>
                     <Button onClick={() => {
-                        if (query) searchTrack(service, query);
+                        if (query) searchTrack(searchServices, query);
                     }}>Search</Button>
                 </div>
                 <ProgressLinear indeterminate={isLoading} color='var(--primary)' value={0} height={6} />
@@ -82,22 +74,23 @@ const SearchView = ({
                         <div className="playlist-track-secondary"><Caption>Artist</Caption></div>
                         <div className="playlist-track-tertiary"><Caption>Album</Caption></div>
                         <div className="playlist-track-duration">
-                            <TimeIcon fill="var(--g-text-color-light"/>
+                            <TimeIcon fill="var(--g-text-color-light" />
                         </div>
                     </div> : ''}
-                    {searchResults ? searchResults.map(track => { return (
-                            <div className={`playlist-track${track.id === currentlyPlaying.id && service === currentlyPlaying.service ? ' playlist-track-active' : ''}`} key={ track.id }>
+                    {searchResults ? searchResults.map(track => {
+                        return (
+                            <div className={`playlist-track${track.id === currentlyPlaying.id && searchServices.includes(currentlyPlaying.service) ? ' playlist-track-active' : ''}`} key={track.id}>
                                 <div className="playlist-track-action">
-                                    { track.id !== currentlyPlaying.id ?
-                                    <IconButton className="playlist-track-play-context" size="small" rounded onClick={() => playContext('spotify', {uris: [`track:${track.id}`]})}>
-                                        <PlayIcon fill='var(--g-text-color-light)'/>
-                                    </IconButton> :
-                                    <IconButton className="playlist-track-pauseplay" size="small" rounded onClick={() => pausePlay(service)}>
-                                        { isPlaying ? 
-                                            <PauseIcon fill='var(--g-text-color-light)'/> :
-                                            <PlayIcon fill='var(--g-text-color-light)'/>
-                                        }
-                                    </IconButton>
+                                    {track.id !== currentlyPlaying.id ?
+                                        <IconButton className="playlist-track-play-context" size="small" rounded onClick={() => playContext('spotify', { uris: [`track:${track.id}`] })}>
+                                            <PlayIcon fill='var(--g-text-color-light)' />
+                                        </IconButton> :
+                                        <IconButton className="playlist-track-pauseplay" size="small" rounded onClick={() => pausePlay(currentlyPlaying.service)}>
+                                            {isPlaying ?
+                                                <PauseIcon fill='var(--g-text-color-light)' /> :
+                                                <PlayIcon fill='var(--g-text-color-light)' />
+                                            }
+                                        </IconButton>
                                     }
                                 </div>
                                 <div className="playlist-track-title">{track.title}</div>
