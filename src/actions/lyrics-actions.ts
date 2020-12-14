f35e406ee40326c5617e7da1e8256a546f78b063
villed3 <villed3@outlook.com>
2020-12-14 15:03:28 +0100

    Moved lyrics module to src and added proxy

--- src/actions/lyrics-actions.ts
+++ src/actions/lyrics-actions.ts
@@ -1,34 +1,7 @@
-import Genius, { Song } from 'genius-lyrics';
+import SongsClient from '../helpers/SongsClient';
+import Song from '../helpers/Song';
 import config from '../genius-config';
-const songsClient = new Genius.SongsClient(config.cilent_access_token, { requestOptions: { params: { access_token: config.cilent_access_token } } } as any);
-
-// export const getCurrentSong = (query: string) => {
-//     return (dispatch, getState) => {
-//         dispatch({ type: 'SET_LYRICS_LOADING' });
-//         dispatch({ type: 'SET_LYRICS_SAME_AS_PLAYING', payload: true });
-//         songsClient.search(query)
-//         .then(songs => {
-//             const song = {
-//                 id: songs[0].id,
-//                 title: songs[0].featuredTitle,
-//                 artist: songs[0].artist.name,
-//                 url: songs[0].url,
-//                 albumArtUrl: songs[0].thumbnail,
-//             }
-//             dispatch({ type: 'SET_CURRENT_LYRICS', payload: song })
-//             console.log(songs[0]);
-
-
-//             return promiseTimeout(songs[0].lyrics(true), 1000).catch(console.error);
-//         })
-//         .then(lyrics => {
-//             dispatch({ type: 'SET_CURRENT_LYRICS', payload: { lyrics } });
-//             dispatch({ type: 'LYRICS_SUCCESS' });
-//             console.log("Loaded lyrics");
-//         })
-//         .catch(handleLyricsError);
-//     }
-// }
+const songsClient = new SongsClient(config.cilent_access_token);
 
 // CREATE HELPER FUNCTION TO REMOVE CODE DUPLICATION IF POSSIBLE
 export const getCurrentLyrics = () => {
@@ -50,12 +23,12 @@ export const getCurrentLyrics = () => {
                 albumArtUrl: songs[0].image,
             };
             dispatch({ type: 'SET_CURRENT_LYRICS', payload: song });
-            console.log(songs[0]);
+            // console.log(songs[0]);
 
             setTimeout(() => songs[0].lyrics(true).then(lyrics => {
                 dispatch({ type: 'SET_CURRENT_LYRICS', payload: { lyrics } });
                 dispatch({ type: 'LYRICS_SUCCESS' });
-                console.log("Test")
+                // console.log("Test")
             }).catch(err => {
                 dispatch({ type: 'LYRICS_ERROR', payload: "Failed to load lyrics. Try reloading." });
                 console.error(err);
@@ -71,6 +44,7 @@ export const getLyricsFromId = (id) => {
         dispatch({ type: 'SET_LYRICS_LOADING' });
         dispatch({ type: 'SET_LYRICS_SAME_AS_PLAYING', payload: false });
         const result: Song = await songsClient.get(id).catch(console.error) as Song;
+        // console.log(result);
         const song = {
             id: result.id,
             title: result.featuredTitle,
@@ -80,9 +54,9 @@ export const getLyricsFromId = (id) => {
         };
         dispatch({ type: 'SET_CURRENT_LYRICS', payload: song });
         setTimeout(() => result.lyrics(true).then(lyrics => {
-            dispatch({ type: 'SET_CURRENT_LYRICS', payload: { lyrics } });
-            dispatch({ type: 'LYRICS_SUCCESS' });
-        })
+                dispatch({ type: 'SET_CURRENT_LYRICS', payload: { lyrics } });
+                dispatch({ type: 'LYRICS_SUCCESS' });
+            })
             .catch(err => {
                 dispatch({ type: 'LYRICS_ERROR', payload: "Failed to load lyrics. Try reloading." });
                 console.error(err);
@@ -95,6 +69,7 @@ export const searchLyrics = (query) => {
         dispatch({ type: 'SET_LYRICS_LOADING' });
         dispatch({ type: 'SET_LYRICS_SAME_AS_PLAYING', payload: false });
         const songs = await songsClient.search(query).then(result => {
+            // console.log(result)
             dispatch({ type: 'LYRICS_SUCCESS' });
             return result;
         }).catch(console.error);
@@ -116,35 +91,3 @@ export const searchLyrics = (query) => {
         else dispatch({ type: 'LYRICS_ERROR', payload: "No results found. Check spelling and try again." });
     }
 }
-
-// export const promiseTimeout = (promise: Promise<any>, time: number) => {
-//     let timeout = new Promise((resolve, reject) => {
-//         let id = setTimeout(() => {
-//             clearTimeout(id);
-//             reject('Timed out in ' + time + 'ms.');
-//         }, time);
-//     })
-//     return Promise.race([promise, timeout])
-// }
-
-// const getSongs = async (query) => {
-//     const searches = await songsClient.search(query);
-//     const firstSong = searches[0];
-//     console.log(firstSong);
-//     const lyrics = await firstSong.lyrics(true);
-//     return {
-//         id: firstSong.id,
-//         title: firstSong.featuredTitle,
-//         artist: firstSong.artist.name,
-//         url: firstSong.url,
-//         albumArtUrl: firstSong.thumbnail,
-//         lyrics
-//     };
-// }
-
-// export const handleLyricsError = err => {
-//     return (dispatch, getState) => {
-//         dispatch({ type: 'LYRICS_ERROR', err });
-//         console.error(err);
-//     }
-// }
