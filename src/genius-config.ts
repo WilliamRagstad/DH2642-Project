f1fe60bde71db10c8cb1148cd81655d9750ad9ac
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2020-12-19 14:04:02 +0100

    Moved services to folder

--- src/genius-config.ts
+++
@@ -1,5 +0,0 @@
-const config = {
-    client_id: "tuqIczlqkwtXU9dSMw7O-zX-R2sRY-mBqn-DWTWjngo_V_jKrVeDFNwbUDhcjoEy",
-    cilent_access_token: "N2krsf4WKzP5qorCyLtU068kSUrMcuOiU3_F9zv_l9-4wVCSJYcX80DYBsHwwq4h",
-}
-export default config;
